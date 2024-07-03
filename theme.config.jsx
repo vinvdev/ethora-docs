9c3a83a029bf36c43c962a3bf8b5b81c9b6793f8
Nikhil Bankar <nikhilbankar1994@gmail.com>
2024-07-03 10:59:43 +0530

    demo content quick start

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,6 @@ export default {
     logo: <span>Ethora - web3 super app</span>,
     project: {
       link: 'https://github.com/dappros/ethora'
-    }
-    // ... other theme options
-  }
+    },
+    logo: <img src="https://www.ethora.com/images/ethora-logo-new.png" alt="Ethora Logo" width="130px" />
+}
