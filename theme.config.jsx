5d7f241d9be1ae8b2a4dc28ce60ac5970f5fd05e
Nikhil Bankar <nikhilbankar1994@gmail.com>
2024-07-08 12:23:52 +0530

    added installation instructions

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,28 @@
 export default {
-    logo: <span>Ethora - web3 super app</span>,
-    project: {
-      link: 'https://github.com/dappros/ethora'
-    },
-    logo: <img src="https://www.ethora.com/images/ethora-logo-new.png" alt="Ethora Logo" width="130px" />
-}
+  project: {
+    link: 'https://github.com/dappros/ethora'
+  },
+  logo: <img src="https://www.ethora.com/images/ethora-logo-new.png" alt="Ethora Logo" width="130px" />,
+  head: (
+    <>
+      <title>Ethora Docs</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      <meta name="description" content="Ethora Documentation" />
+      <meta name="og:title" content="Ethora Docs" />
+      <link rel="icon" href="https://www.ethora.com/images/favicon_io/favicon.ico" />
+    </>
+  ),
+
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://www.ethora.com" target="_blank">
+          Ethora
+        </a>
+        .
+      </span>
+    )
+  }
+};
+
